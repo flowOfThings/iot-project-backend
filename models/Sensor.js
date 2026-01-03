c1a2605543e188ed6c71013596cff4a675eca4b3
Karri <karrit@proton.me>
2026-01-03 11:36:08 +0200

    Add Sensor.js exp

--- models/Sensor.js
+++ models/Sensor.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const sensorSchema = new mongoose.Schema({
   timestamp: Number,
   temperature: Number,
-  humidity: Number
+  humidity: Number,
+  exp: Number
 });
 
 module.exports = mongoose.model("Sensor", sensorSchema);
